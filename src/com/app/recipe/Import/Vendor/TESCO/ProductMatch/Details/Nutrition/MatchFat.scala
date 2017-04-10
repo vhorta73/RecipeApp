b586a8b4ac6e5daf72f06249c0e121932f4a5309
Vasco Horta <diasfonseca@live.com>
2017-04-10 19:15:17 +0100

    Adding the Fat() rules

--- src/com/app/recipe/Import/Vendor/TESCO/ProductMatch/Details/Nutrition/MatchFat.scala
+++ src/com/app/recipe/Import/Vendor/TESCO/ProductMatch/Details/Nutrition/MatchFat.scala
@@ -1,10 +1,10 @@
 package com.app.recipe.Import.Vendor.TESCO.ProductMatch.Details.Nutrition
 
-import com.app.recipe.Import.Product.Model.Fat
 import com.app.recipe.Import.Product.Model.ProductNutrition
 import com.app.recipe.Import.Product.Units.Model.StandardUnits
 import com.app.recipe.Import.Product.Units.Model.StandardUnits.Units
 import com.app.recipe.String.Utils._
+import com.app.recipe.Import.Product.Model.Fat
 
 /**
  * Class to find match the fat values from a given line to parse.
@@ -12,29 +12,109 @@ import com.app.recipe.String.Utils._
  * return more than one element.
  */
 class MatchFat() {
+  
+  /**
+   * Defining what states a string line contains fat information.
+   */
+  private final val FAT_STRING : String = "fat"
+
+  /**
+   * All regex that will retrieve the fat value from a given string.
+   */
+  // Matching: <th scope="row">Fat (g)</th><td>10.0</td><td>5.0</td><td>-</td><td>-</td></tr>
+  private final val FAT_G_01_REGEX = """<th scope="row">Fat \(g\)</th><td>([0-9.]+)</td>""".r.unanchored
+  // Matching: <th scope="row">Fat</th><td>11.0g</td>
+  private final val FAT_G_02_REGEX = """<th scope="row">Fat</th><td>([0-9.]+)g</td>""".r.unanchored
+  // Matching: <th scope="row">Fats (g)</th><td>4.8</td>
+  private final val FAT_G_03_REGEX = """<th scope="row">Fats \(g\)</th><td>([0-9.]+)</td>""".r.unanchored
+  // Matching: <th scope="row">Fat:</th><td>0g</td>
+  private final val FAT_G_04_REGEX = """<th scope="row">Fat:</th><td>([0-9.]+)g</td>""".r.unanchored
+  // Matching: <th scope="row">Fat:</th><td>< 0.5 g</td>
+  private final val FAT_G_05_REGEX = """<th scope="row">Fat:</th><td>< ([0-9.]+) g</td>""".r.unanchored
+  // Matching: <th scope="row">Fat</th><td>Negligible amount</td>
+  private final val FAT_G_06_NEGLIGIBLE_REGEX = """<th scope="row">Fat</th><td>[a-zA-Z ]+(egligible amount)[a-zA-Z ]+</td>""".r.unanchored
+  // Matching: <th scope="row">Fat</th><td>1.5 g</td>
+  private final val FAT_G_07_REGEX = """<th scope="row">Fat</th><td>([0-9.]+) g</td>""".r.unanchored
+  // Matching: <th scope="row">Contains negligible amounts of - Fat, Saturates and Protein. Electrolytes per 100ml: Sodium 50mg</th><td>-</td>
+  private final val FAT_G_08_NEGLIGIBLE_REGEX = """<th scope="row">[a-zA-z ]+(negligible)[^0-9.]+.*</th>""".r.unanchored
+  // Matching: <th scope="row">Fat:</th><td><0.5 g</td></tr>
+  private final val FAT_G_09_REGEX = """<th scope="row">Fat:</th><td><([0-9.]+) g</td>""".r.unanchored
+  // Matching: <th scope="row">Fat</th><td>Negligible amount</td><td>Negligible amount</td></tr>
+  private final val FAT_G_10_NEGLIGIBLE_REGEX = """<th scope="row">Fat</th><td>(Negligible amount)</td>""".r.unanchored
+  // Matching: <th scope="row">Fat</th><td>< 0.1g</td></tr>
+  private final val FAT_G_11_REGEX = """<th scope="row">Fat</th><td>< ([0-9.]+)g</td></tr>""".r.unanchored
+  // Matching: <th scope="row">Fat, saturates, protein - negligible amount</th><td>-</td>
+  private final val FAT_G_12_NEGLIGIBLE_REGEX = """<th scope="row">Fat, saturates, protein - (negligible) amount</th>""".r.unanchored
+  // Matching: <th scope="row">Fat</th><td>Negligible Amount</td><td>Negligible Amount</td></tr>
+  private final val FAT_G_13_NEGLIGIBLE_REGEX = """<th scope="row">Fat</th><td>(Negligible) Amount</td>""".r.unanchored
+  // Matching: <th scope="row">Fat</th><td><0.1g</td>
+  private final val FAT_G_14_REGEX = """<th scope="row">Fat</th><td><([0-9.]+)g</td>""".r.unanchored
+  // Matching: <th scope="row">Trans Fatty Acids</th><td>0.1g</td>
+  private final val FAT_G_15_REGEX = """<th scope="row">Trans Fatty Acids</th><td>([0-9.]+)g</td>""".r.unanchored
+  // Matching: <th scope="row">Fat</th><td>Neglibible amount</td><td>Neglibible amount</td>
+  private final val FAT_G_16_NEGLIGIBLE_REGEX = """<th scope="row">Fat</th><td>(Neglibible) amount</td><td>Neglibible amount</td>""".r.unanchored
+  // Matching: <th scope="row">Fat 1</th><td>1.5 g</td><td>2%</td>
+  private final val FAT_G_17_REGEX = """<th scope="row">Fat 1</th><td>([0-9.]+) g</td>""".r.unanchored
+  // Matching: <th scope="row">Fat, g</th><td>0</td><td>0</td>
+  private final val FAT_G_18_REGEX = """<th scope="row">Fat, g</th><td>([0-9.]+)</td>""".r.unanchored
+  // Matching: <th scope="row">Fat g</th><td>7</td><td>23</td>
+  private final val FAT_G_19_REGEX = """<th scope="row">Fat g</th><td>([0-9.]+)</td>""".r.unanchored
+  // Matching: <th scope="row">Fat</th><td><0,5 g</td>
+  private final val FAT_G_20_COMMA_REGEX = """<th scope="row">Fat</th><td><([0-9.,]+) g</td>""".r.unanchored
+  // Matching: <th scope="row">Fat</th><td>0,2 g</td>
+  private final val FAT_G_21_COMMA_REGEX = """<th scope="row">Fat</th><td>([0-9.,]+) g</td>""".r.unanchored
+  // Matching: <th scope="row">Fat</th><td>negligible amount</td>
+  private final val FAT_G_22_NEGLIGIBLE_REGEX = """<th scope="row">Fat</th><td>(negligible) amount</td>""".r.unanchored
+  // Matching: <th scope="row">Total Fat</th><td>23.2g</td>
+  private final val FAT_G_23_REGEX = """<th scope="row">Total Fat</th><td>([0-9.]+)g</td>""".r.unanchored
+  // Matching: <th scope="row">Fat</th><td>trace</td><td>trace</td>
+  private final val FAT_G_24_TRACE_REGEX = """<th scope="row">Fat</th><td>(trace)</td>""".r.unanchored
+  // Matching: <th scope="row">Fat</th><td>9.9g(14%*)</td><td>30.5g</td></tr>
+  private final val FAT_G_25_REGEX = """<th scope="row">Fat</th><td>([0-9.]+)g\([0-9%*]+\)</td>""".r.unanchored
+  // Matching: <th scope="row">Fat - total</th><td>0 g</td>
+  private final val FAT_G_26_REGEX = """<th scope="row">Fat - total</th><td>([0-9.]+) g</td>""".r.unanchored
+  // Matching: negligible
+  private final val FAT_G_27_NEGLIGIBLE_REGEX = """(negligible)""".r.unanchored
+
   /**
    * Returns the Fat case class with the detailed values as displaying on the web page.
    */
   def getMatch(productString : String) : List[ProductNutrition] = {
-    // regex for finding the fat values per 100g
-    val valuesRegex = """(?<=td>)([0-9<>.]+)[^</td|^g<]*""".r
 
-    // Interested only in finding the first match.
-    val valuesMatch = valuesRegex.findFirstMatchIn(productString)
-
-    // If empty, this is sign of a bad matching and needs to be revised.
-    if ( valuesMatch.isEmpty )
-      throw new IllegalStateException("No Fat values matched line: ["+productString+"]")
-
-    // Find out which units to use.
-    var energyUnits : Units = getUnit(productString)
-
-    // If units did not match, a new rule is required
-    if ( energyUnits == null )
-      throw new IllegalStateException("No Fat units matched line: ["+productString+"]")
-
-    // Return first match as a list.
-    return List(Fat(valuesMatch.get.toString().toQuantity,energyUnits))
+    // Adding all elements to this list which is to be returned last.
+    var finalList : List[ProductNutrition] = Nil
+    
+    productString match {
+      case FAT_G_01_REGEX(fat)                   => finalList = finalList ::: List(Fat(fat.toDouble,StandardUnits.g))
+      case FAT_G_02_REGEX(fat)                   => finalList = finalList ::: List(Fat(fat.toDouble,StandardUnits.g))
+      case FAT_G_03_REGEX(fat)                   => finalList = finalList ::: List(Fat(fat.toDouble,StandardUnits.g))
+      case FAT_G_04_REGEX(fat)                   => finalList = finalList ::: List(Fat(fat.toDouble,StandardUnits.g))
+      case FAT_G_05_REGEX(fat)                   => finalList = finalList ::: List(Fat(fat.toDouble,StandardUnits.g))
+      case FAT_G_06_NEGLIGIBLE_REGEX(negligible) => finalList = finalList ::: List(Fat(0,StandardUnits.g))
+      case FAT_G_07_REGEX(fat)                   => finalList = finalList ::: List(Fat(fat.toDouble,StandardUnits.g))
+      case FAT_G_08_NEGLIGIBLE_REGEX(negligible) => finalList = finalList ::: List(Fat(0,StandardUnits.g))
+      case FAT_G_09_REGEX(fat)                   => finalList = finalList ::: List(Fat(fat.toDouble,StandardUnits.g))
+      case FAT_G_10_NEGLIGIBLE_REGEX(negligible) => finalList = finalList ::: List(Fat(0,StandardUnits.g))
+      case FAT_G_11_REGEX(fat)                   => finalList = finalList ::: List(Fat(fat.toDouble,StandardUnits.g))
+      case FAT_G_12_NEGLIGIBLE_REGEX(negligible) => finalList = finalList ::: List(Fat(0,StandardUnits.g))
+      case FAT_G_13_NEGLIGIBLE_REGEX(negligible) => finalList = finalList ::: List(Fat(0,StandardUnits.g))
+      case FAT_G_14_REGEX(fat)                   => finalList = finalList ::: List(Fat(fat.toDouble,StandardUnits.g))
+      case FAT_G_15_REGEX(fat)                   => finalList = finalList ::: List(Fat(fat.toDouble,StandardUnits.g))
+      case FAT_G_16_NEGLIGIBLE_REGEX(negligible) => finalList = finalList ::: List(Fat(0,StandardUnits.g))
+      case FAT_G_17_REGEX(fat)                   => finalList = finalList ::: List(Fat(fat.toDouble,StandardUnits.g))
+      case FAT_G_18_REGEX(fat)                   => finalList = finalList ::: List(Fat(fat.toDouble,StandardUnits.g))
+      case FAT_G_19_REGEX(fat)                   => finalList = finalList ::: List(Fat(fat.toDouble,StandardUnits.g))
+      case FAT_G_20_COMMA_REGEX(fat)             => finalList = finalList ::: List(Fat(fat.replaceAll(",", "").toDouble,StandardUnits.g))
+      case FAT_G_21_COMMA_REGEX(fat)             => finalList = finalList ::: List(Fat(fat.replaceAll(",", "").toDouble,StandardUnits.g))
+      case FAT_G_22_NEGLIGIBLE_REGEX(negligible) => finalList = finalList ::: List(Fat(0,StandardUnits.g))
+      case FAT_G_23_REGEX(fat)                   => finalList = finalList ::: List(Fat(fat.toDouble,StandardUnits.g))
+      case FAT_G_24_TRACE_REGEX(trace)           => finalList = finalList ::: List(Fat(0,StandardUnits.g))
+      case FAT_G_25_REGEX(fat)                   => finalList = finalList ::: List(Fat(fat.toDouble,StandardUnits.g))
+      case FAT_G_26_REGEX(fat)                   => finalList = finalList ::: List(Fat(fat.toDouble,StandardUnits.g))
+      case FAT_G_27_NEGLIGIBLE_REGEX(negligible) => finalList = finalList ::: List(Fat(0,StandardUnits.g))
+      case _ => warn(s"No Fat Matched $productString")
+    }
+    return finalList
   }
 
   /**
@@ -43,19 +123,8 @@ class MatchFat() {
    */
   def isFat(productString : String) : Boolean = {
     productString match {
-      case x if x.contains("Fat") => true
+      case x if x.toLowerCase().contains(FAT_STRING) => true
       case _ => false
-    }
-  }
-  
-  /**
-   * Look for possible fat units. Usually is represented in grams.
-   */
-  private final def getUnit(productString : String) : Units = {
-    productString match {
-      case x if x.contains("(g)") => StandardUnits.g
-      case x if x.contains("g<") => StandardUnits.g
-      case _ => throw new IllegalArgumentException("No Fat unit found in line: ["+productString+"]")
     }
   }
 }
