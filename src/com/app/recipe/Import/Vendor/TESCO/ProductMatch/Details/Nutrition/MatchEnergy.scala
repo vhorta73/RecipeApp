48f91bd649e5e9fabc5d5d5244cdced8cd4e8f9c
Vasco Horta <diasfonseca@live.com>
2017-04-10 16:28:45 +0100

    More energy controlled regex

--- src/com/app/recipe/Import/Vendor/TESCO/ProductMatch/Details/Nutrition/MatchEnergy.scala
+++ src/com/app/recipe/Import/Vendor/TESCO/ProductMatch/Details/Nutrition/MatchEnergy.scala
@@ -20,10 +20,11 @@ class MatchEnergy() extends RecipeLogging {
    * The strings to be used on defining if supplied string contains energy 
    * information or not.
    */
-  private final val INTAKE_STRING : String = "ence intake "
-  private final val ENERGY_STRING : String = "energy"
-  private final val KCAL_STRING   : String = "kal"
-  private final val KJ_STRING     : String = "kj"
+  private final val INTAKE_STRING  : String = "ence intake "
+  private final val ENERGY_STRING  : String = "energy"
+  private final val KCAL_STRING    : String = "kal"
+  private final val KJ_STRING      : String = "kj"
+  private final val PLANT_STEROLDS : String = "plant sterols"
 
   /**
    * The regex constants to extract the energy values from supplied string.
@@ -52,6 +53,16 @@ class MatchEnergy() extends RecipeLogging {
   private final val KJ_11_REGEX = """<th scope="row">Energy:</th><td>([0-9.]+)kJ</td>""".r.unanchored
   // Matching: <th scope="row">Energy</th><td>641 KJ</td>
   private final val KJ_12_REGEX = """<th scope="row">Energy</th><td>([0-9.]+) KJ</td>""".r.unanchored
+  // Matching: <th scope="row">Energy kJ</th><td>247kJ</td>
+  private final val KJ_13_REGEX = """<th scope="row">Energy kJ</th><td>([0-9.]+)kJ</td>""".r.unanchored
+  // Matching: <th scope="row">-</th><td>970 kJ</td>
+  private final val KJ_14_REGEX = """<th scope="row">-</th><td>([0-9.]+) kJ</td>""".r.unanchored
+  // Matching: <th scope="row">Energy</th><td>105kJ/</td>
+  private final val KJ_15_REGEX = """<th scope="row">Energy</th><td>([0-9.]+)kJ/</td>""".r.unanchored
+  // Matching: <th scope="row">-</th><td>1588kJ</td>
+  private final val KJ_16_REGEX = """<th scope="row">-</th><td>([0-9.]+)kJ</td>""".r.unanchored
+  // Matching: <th scope="row">Energy Value kJ</th><td>Trace</td>
+  private final val KJ_17_TRACE_REGEX = """<th scope="row">Energy Value kJ</th><td>(Trace)</td>""".r.unanchored
 
   // Matching: <th scope="row">Energy kCal</th><td>400.0</td>
   private final val KCAL_01_REGEX = """<th scope="row">Energy kCal</th><td>([0-9.]+)</td>""".r.unanchored
@@ -63,7 +74,13 @@ class MatchEnergy() extends RecipeLogging {
   private final val KCAL_04_REGEX = """<th scope="row">Energy \(kcals\)</th><td>([0-9.]+)</td>""".r.unanchored
   // Matching: <th scope="row">Energy kcal</th><td>357</td>
   private final val KCAL_05_REGEX = """<th scope="row">Energy kcal</th><td>([0-9.]+)</td>""".r.unanchored
-  
+  // Matching: <th scope="row">Energy (Kcal)</th><td>483</td>
+  private final val KCAL_06_REGEX = """<th scope="row">Energy \(Kcal\)</th><td>([0-9.]+)</td>""".r.unanchored
+  // Matching: <th scope="row">Energy kcal</th><td>58kcal</td>
+  private final val KCAL_07_REGEX = """<th scope="row">Energy kcal</th><td>([0-9.]+)kcal</td>""".r.unanchored
+  // Matching: <th scope="row">Energy</th><td>233 kcal</td>
+  private final val KCAL_08_REGEX = """<th scope="row">Energy</th><td>([0-9.]+) kcal</td>""".r.unanchored
+
   // Matching: <th scope="row">Energy</th><td>3kJ/1kcal</td>
   private final val KJ_KCAL_01_REGEX = """<th scope="row">Energy</th><td>([0-9.]+)kJ/([0-9.]+)kcal</td>""".r.unanchored
   // Matching: <th scope="row">Energy (kJ/kcal)</th><td>1523/364</td>
@@ -100,6 +117,40 @@ class MatchEnergy() extends RecipeLogging {
   private final val KJ_KCAL_17_REGEX = """<th scope="row">Energy:</th><td>([0-9.]+)kJ/([0-9.]+)kcal</td>""".r.unanchored
   // Matching: <th scope="row">Energy</th><td>105 kJ/ 25 kcal</td>
   private final val KJ_KCAL_18_REGEX = """<th scope="row">Energy</th><td>([0-9.]+) kJ/ ([0-9.]+) kcal</td>""".r.unanchored
+  // Matching: <th scope="row">Energy</th><td>60/14</td>
+  private final val KJ_KCAL_19_REGEX = """<th scope="row">Energy</th><td>([0-9.]+)/([0-9.]+)</td>""".r.unanchored
+  // Matching: <th scope="row">Energy kJ/kcal</th><td>349/82</td>
+  private final val KJ_KCAL_20_REGEX = """<th scope="row">Energy kJ/kcal</th><td>([0-9.]+)/([0-9.]+)</td>""".r.unanchored
+  // Matching: <th scope="row">Energy</th><td>239KJ/57Kcal</td>
+  private final val KJ_KCAL_21_REGEX = """<th scope="row">Energy</th><td>([0-9.]+)KJ/([0-9.]+)Kcal</td>""".r.unanchored
+  // Matching: <th scope="row">Energy kJ/kcal</th><td>93kJ/22kcal</td>
+  private final val KJ_KCAL_22_REGEX = """<th scope="row">Energy kJ/kcal</th><td>([0-9.]+)kJ/([0-9.]+)kcal</td>""".r.unanchored
+  // Matching: <th scope="row">Energy:</th><td>325 kcal - 1360 kJ</td>
+  private final val KJ_KCAL_23_REGEX = """<th scope="row">Energy:</th><td>([0-9.]+) kcal - ([0-9.]+) kJ</td>""".r.unanchored
+  // Matching: <th scope="row">Energy, kJ/kcal</th><td>423 / 100</td>
+  private final val KJ_KCAL_24_REGEX = """<th scope="row">Energy, kJ/kcal</th><td>([0-9.]+) / ([0-9.]+)</td>""".r.unanchored
+  // Matching: <th scope="row">Energy (kJ/kcal)</th><td>234/ 55</td>
+  private final val KJ_KCAL_25_REGEX = """<th scope="row">Energy \(kJ/kcal\)</th><td>([0-9.]+)/ ([0-9.]+)</td>""".r.unanchored
+  // Matching: <th scope="row">Energy</th><td>160KJ40Kcal</td>
+  private final val KJ_KCAL_26_REGEX = """<th scope="row">Energy</th><td>([0-9.]+)KJ([0-9.]+)Kcal</td>""".r.unanchored
+  // Matching: <th scope="row">Energy (kJ/kcal)</th><td>121/28kcal</td>
+  private final val KJ_KCAL_27_REGEX = """<th scope="row">Energy \(kJ/kcal\)</th><td>([0-9.]+)/([0-9.]+)kcal</td>""".r.unanchored
+  // Matching: <th scope="row">Energy</th><td>239KJ/57kcal</td>
+  private final val KJ_KCAL_28_REGEX = """<th scope="row">Energy</th><td>([0-9.]+)KJ/([0-9.]+)kcal</td>""".r.unanchored
+  // Matching: <th scope="row">Energy (kJ/kcal)</th><td>232/ 55</td>
+  private final val KJ_KCAL_29_REGEX = """<th scope="row">Energy \(kJ/kcal\)</th><td>([0-9.]+)/ ([0-9.]+)</td>""".r.unanchored
+  // Matching: <th scope="row">Energ</th><td>217kJ (51kcal)</td>
+  private final val KJ_KCAL_30_REGEX = """<th scope="row">Energ</th><td>([0-9.]+)kJ \(([0-9.]+)kcal\)</td>""".r.unanchored
+  // Matching: <th scope="row">Energy</th><td>160KJ/40kcal</td>
+  private final val KJ_KCAL_31_REGEX = """<th scope="row">Energy</th><td>([0-9.]+)KJ/([0-9.]+)kcal</td>""".r.unanchored
+  // Matching: <th scope="row">Energy</th><td>0 kJ/kcal</td>
+  private final val KJ_KCAL_32_SAME_REGEX = """<th scope="row">Energy</th><td>([0-9.]+) kJ/kcal</td>""".r.unanchored
+  // Matching: <th scope="row">Energy</th><td>9.6kcal/40kJ</td>
+  private final val KJ_KCAL_33_REGEX = """<th scope="row">Energy</th><td>([0-9.]+)kcal/([0-9.]+)kJ</td>""".r.unanchored
+  // Matching: <th scope="row">Energy value kJ/(kcal)</th><td>0/(0)</td>
+  private final val KJ_KCAL_34_REGEX = """<th scope="row">Energy value kJ/\(kcal\)</th><td>([0-9.]+)/\(([0-9.]+)\)</td>""".r.unanchored
+  // Matching: <th scope="row">Energy</th><td>8.4 kJ/ 2.0kcal</td>
+  private final val KJ_KCAL_35_REGEX = """<th scope="row">Energy</th><td>([0-9.]+) [a-zA-Z]+/ ([0-9.]+)[a-zA-Z]+</td>""".r.unanchored
 
   /**
    * Returns the Energy case class with the detailed values as displaying on the web page.
@@ -123,6 +174,11 @@ class MatchEnergy() extends RecipeLogging {
       case KJ_10_REGEX(kjDouble)                 => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj))
       case KJ_11_REGEX(kjDouble)                 => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj))
       case KJ_12_REGEX(kjDouble)                 => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj))
+      case KJ_13_REGEX(kjDouble)                 => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj))
+      case KJ_14_REGEX(kjDouble)                 => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj))
+      case KJ_15_REGEX(kjDouble)                 => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj))
+      case KJ_16_REGEX(kjDouble)                 => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj))
+      case KJ_17_TRACE_REGEX(trace)              => finalList = finalList ::: List(Energy(0,Kj),Energy(0,Kcal))
 
       // Matching on Kcal
       case KCAL_01_REGEX(kcalDouble)             => finalList = finalList ::: List(Energy(kcalDouble.toDouble,Kcal))
@@ -130,6 +186,9 @@ class MatchEnergy() extends RecipeLogging {
       case KCAL_03_REGEX(kcalDouble)             => finalList = finalList ::: List(Energy(kcalDouble.toDouble,Kcal))
       case KCAL_04_REGEX(kcalDouble)             => finalList = finalList ::: List(Energy(kcalDouble.toDouble,Kcal))
       case KCAL_05_REGEX(kcalDouble)             => finalList = finalList ::: List(Energy(kcalDouble.toDouble,Kcal))
+      case KCAL_06_REGEX(kcalDouble)             => finalList = finalList ::: List(Energy(kcalDouble.toDouble,Kcal))
+      case KCAL_07_REGEX(kcalDouble)             => finalList = finalList ::: List(Energy(kcalDouble.toDouble,Kcal))
+      case KCAL_08_REGEX(kcalDouble)             => finalList = finalList ::: List(Energy(kcalDouble.toDouble,Kcal))
 
       // Matching on Kj Kcal
       case KJ_KCAL_01_REGEX(kjDouble,kcalDouble) => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj),Energy(kcalDouble.toDouble,Kcal))
@@ -150,6 +209,23 @@ class MatchEnergy() extends RecipeLogging {
       case KJ_KCAL_16_REGEX(kjDouble,kcalDouble) => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj),Energy(kcalDouble.toDouble,Kcal))
       case KJ_KCAL_17_REGEX(kjDouble,kcalDouble) => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj),Energy(kcalDouble.toDouble,Kcal))
       case KJ_KCAL_18_REGEX(kjDouble,kcalDouble) => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj),Energy(kcalDouble.toDouble,Kcal))
+      case KJ_KCAL_19_REGEX(kjDouble,kcalDouble) => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj),Energy(kcalDouble.toDouble,Kcal))
+      case KJ_KCAL_20_REGEX(kjDouble,kcalDouble) => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj),Energy(kcalDouble.toDouble,Kcal))
+      case KJ_KCAL_21_REGEX(kjDouble,kcalDouble) => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj),Energy(kcalDouble.toDouble,Kcal))
+      case KJ_KCAL_22_REGEX(kjDouble,kcalDouble) => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj),Energy(kcalDouble.toDouble,Kcal))
+      case KJ_KCAL_23_REGEX(kjDouble,kcalDouble) => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj),Energy(kcalDouble.toDouble,Kcal))
+      case KJ_KCAL_24_REGEX(kjDouble,kcalDouble) => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj),Energy(kcalDouble.toDouble,Kcal))
+      case KJ_KCAL_25_REGEX(kjDouble,kcalDouble) => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj),Energy(kcalDouble.toDouble,Kcal))
+      case KJ_KCAL_26_REGEX(kjDouble,kcalDouble) => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj),Energy(kcalDouble.toDouble,Kcal))
+      case KJ_KCAL_27_REGEX(kjDouble,kcalDouble) => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj),Energy(kcalDouble.toDouble,Kcal))
+      case KJ_KCAL_28_REGEX(kjDouble,kcalDouble) => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj),Energy(kcalDouble.toDouble,Kcal))
+      case KJ_KCAL_29_REGEX(kjDouble,kcalDouble) => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj),Energy(kcalDouble.toDouble,Kcal))
+      case KJ_KCAL_30_REGEX(kjDouble,kcalDouble) => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj),Energy(kcalDouble.toDouble,Kcal))
+      case KJ_KCAL_31_REGEX(kjDouble,kcalDouble) => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj),Energy(kcalDouble.toDouble,Kcal))
+      case KJ_KCAL_32_SAME_REGEX(sameDouble) => finalList = finalList ::: List(Energy(sameDouble.toDouble,Kj),Energy(sameDouble.toDouble,Kcal))
+      case KJ_KCAL_33_REGEX(kjDouble,kcalDouble) => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj),Energy(kcalDouble.toDouble,Kcal))
+      case KJ_KCAL_34_REGEX(kjDouble,kcalDouble) => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj),Energy(kcalDouble.toDouble,Kcal))
+      case KJ_KCAL_35_REGEX(kjDouble,kcalDouble) => finalList = finalList ::: List(Energy(kjDouble.toDouble,Kj),Energy(kcalDouble.toDouble,Kcal))
       case _ => warn(s"No Energy Matched $productString")
     }
 
@@ -162,10 +238,16 @@ class MatchEnergy() extends RecipeLogging {
    */
   def isEnergy(productString : String) : Boolean = {
     productString match {
-      case line if line.toLowerCase().contains(INTAKE_STRING) => false
-      case line if line.toLowerCase().contains(ENERGY_STRING) => true
-      case line if line.toLowerCase().contains(KCAL_STRING)   => true
-      case line if line.toLowerCase().contains(KJ_STRING)     => true
+      // These may contain energy references but are not the line of interest.
+      case line if line.toLowerCase().contains(INTAKE_STRING)  => false
+      case line if line.toLowerCase().contains(PLANT_STEROLDS) => false
+      
+      // Expected energy lines.
+      case line if line.toLowerCase().contains(ENERGY_STRING)  => true
+      case line if line.toLowerCase().contains(KCAL_STRING)    => true
+      case line if line.toLowerCase().contains(KJ_STRING)      => true
+      
+      // All the rest.
       case _ => false
     }
   }
